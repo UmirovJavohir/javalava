99fd9725ece21a1096e66bbef0f0e6b36bf51b8e
UmirovJavohir <email@vulkjava.com>
2025-11-30 23:49:18 +0500

    using twist for command

--- git_organish.py
+++ git_organish.py
@@ -1,1 +1,12 @@
-print("salom")
+# rows==строка
+# columns==столбец
+# symbol==символ
+
+stroka=int(input("введите кол-во строк: "))
+stolbets=int(input("введите кол-во столбец: "))
+symvol=input("введите кол-во символ: ")
+
+for q in range(stroka):
+    for w in range(stolbets):
+        print(symvol, end=" ")
+    print()
